82d3744cdba2db66ce015a028da5fc7663e5e295
siri-ponnapalli <ponnapallisirichandana@gmail.com>
2024-10-09 19:02:38 +0530

    expense-terraform

---
+++ variables.tf
@@ -0,0 +1,43 @@
+variable "project_name" {
+    default = "expense"
+}
+
+variable "environment" {
+    default = "dev"
+}
+variable "vpc_cidr" {
+    default = "172.0.0.0/16"
+}
+
+
+variable "common_tags" {
+    default = {
+        project = "expense"
+        Terraform = "true"
+        Environment = "dev"
+    } 
+}
+
+variable "public_subnet_cidrs" {
+    default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+    default = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+
+
+variable "database_subnet_cidrs" {
+    default = ["10.0.21.0/24", "10.0.22.0/24"]
+}
+
+variable "is_peering_requried" {
+    default = true
+  
+}
+
+variable "mysql_sg_tags" {
+    default = {
+        component = "mysql"
+    }
+}
